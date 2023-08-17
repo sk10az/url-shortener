26c2be48460911731e2e98fe23c42dadacac2ee7
lok4lz <sk10azik@gmail.com>
2023-08-17 20:11:45 +0300

    first commit

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-Default:"local" env-required:"true"` // TODO: можно удалить зависимость
+	StoragePath string `yaml:"storage_path" env_required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env_default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env_default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env_default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
